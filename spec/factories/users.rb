35aedfeac4e68803911750acea296d1ffda3d839
Nathan <admin@bigrocketgames.com>
2017-07-06 20:20:56 -0700

    Adding user model with generator.  Setup postgres db.

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+  factory :user do
+    email "MyString"
+    password_digest "MyString"
+    first_name "MyString"
+    last_name "MyString"
+    city "MyString"
+    state "MyString"
+  end
+end
