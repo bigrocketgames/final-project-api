09f092905e6dc0944405803882ee446e39804f4b
Nathan Robbert <admin@bigrocketgames.com>
2017-07-08 08:03:00 -0700

    Updated user_spec tests.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,19 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
 
-    it 'requires an email and password upon creation'
+    it 'requires an email and password upon creation' do
+      user = build(:user, email: nil, password: nil)
+
+      expect(user.valid?).to eq(false)
+      expect(user.errors.full_messages).to eq([
+        "Password can't be blank",
+        "Email can't be blank"
+      ])
+    end
 
     it 'requires that an email is unique'
+
+    it 'requires that an email is valid (contains an @ symbol and a (.com, .org, etc...)'
 
   end
 
