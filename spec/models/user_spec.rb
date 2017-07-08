272c35c059d1e09782b7d3657e0c3fff9a901ac2
Nathan Robbert <admin@bigrocketgames.com>
2017-07-08 08:19:48 -0700

    Updated user model spec to pass verfication of hashed password.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -38,7 +38,12 @@ RSpec.describe User, type: :model do
 
   describe 'on save' do
 
-    it 'hashes a password'
+    it 'hashes a password' do
+      user = build(:user)
+      user.save
+
+      expect(user.password_digest).not_to eq(user.password)
+    end
 
   end
 
