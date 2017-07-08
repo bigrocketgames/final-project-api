d6415d5a94da458e8b37038420e06d042d71c1ee
Nathan Robbert <admin@bigrocketgames.com>
2017-07-08 07:52:13 -0700

    update user specs.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+
+    it 'requires an email and password upon creation'
+
+    it 'requires that an email is unique'
+
+  end
+
+  describe 'on save' do
+
+    it 'hashes a password'
+
+  end
+
+  describe 'relationships' do
+
+    it 'has many favorite teams'
+
+    it 'has many friends'
+
+    it 'has many messages'
+
+  end
+
 end
