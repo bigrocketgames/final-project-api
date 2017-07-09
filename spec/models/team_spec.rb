212efd327d9dda5361ce9f145b7505bac9e1cab3
Nathan Robbert <admin@bigrocketgames.com>
2017-07-09 13:58:36 -0700

    Working on specs for validations and relationships on the team model.

---
+++ spec/models/team_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe Team, type: :model do
+  describe 'validations' do
+    it 'requires a name, mascot, and sport upon creation' do
+      team = build(:team, name: nil, mascot: nil, sport: nil)
+
+      expect(team.valid?).to eq(false)
+      expect(team.errors.full_messages).to eq([
+        "Sport must exist",
+        "Sport can't be blank",
+        "Name can't be blank",
+        "Mascot can't be blank"
+      ])
+    end
+    
+  end
+  
+  describe 'relationships' do
+    it 'belongs to a sport' do
+      team = build(:team)
+
+      expect(team.sport).to_not eq(nil)
+    end
+
+    it 'has many fans through favorite teams'
+    
+  end
+  
+end
