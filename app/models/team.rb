02655d8b675156f77b2c6047d88e67ff0aa8acc5
Nathan Robbert <admin@bigrocketgames.com>
2017-07-09 17:33:47 -0700

    Added custom validation method for making sure no duplicate teams in the same sport.

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,12 @@ class Team < ApplicationRecord
   belongs_to :sport
 
   validates :name, :mascot, :sport, presence: true
+  validate :no_duplicate_team
+
+  def no_duplicate_team
+    teamExists = Team.where('name = ? AND mascot = ? AND sport_id = ?', self.name, self.mascot, self.sport).count
+    if teamExists > 0
+      errors.add(:name, "can't have a duplicate team")
+    end
+  end
 end
