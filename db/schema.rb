abd3ff91189b19000cb52131afbec834e6cb52b4
Nathan Robbert <admin@bigrocketgames.com>
2017-07-10 08:29:04 -0700

    Created chatroom table and migrated it to the db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170710044559) do
+ActiveRecord::Schema.define(version: 20170710152616) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chat_rooms", force: :cascade do |t|
+    t.integer  "game_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "messages", force: :cascade do |t|
     t.text     "content"
