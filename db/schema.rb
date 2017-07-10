47836e980a78632f55dae5b01e3d2107c71de56c
Nathan Robbert <admin@bigrocketgames.com>
2017-07-09 22:16:16 -0700

    Created migration for messages table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170709200315) do
+ActiveRecord::Schema.define(version: 20170710044559) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "messages", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "chatRoom_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "sports", force: :cascade do |t|
     t.string   "sport_name"
