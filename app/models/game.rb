5d4d63416fa3fb74daf4156c76e9cf5acdeafe1c
Nathan Robbert <admin@bigrocketgames.com>
2017-07-22 16:09:35 -0700

    Updated Game model relationships and validations.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,7 @@
 class Game < ApplicationRecord
+  has_one :chat_room
+  belongs_to :home_team, class_name: "Team"
+  belongs_to :away_team, class_name: "Team"
+
+  validates :game_time, :chat_room_id, :home_team_id, :away_team_id, presence: true
 end
