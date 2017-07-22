b2149529966503f2b8044cfa2d067588b7033af5
Nathan Robbert <admin@bigrocketgames.com>
2017-07-22 14:59:22 -0700

    Establishing beggining steps of chat room specs.

--- spec/models/chat_room_spec.rb
+++ spec/models/chat_room_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe ChatRoom, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  descirbe 'validations' do
+
+  end
+  
+  describe 'relationships' do
+
+  end
+  
 end
