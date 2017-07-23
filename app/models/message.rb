71c6d80adb713db81e31e9f067390896ed4e9311
Nathan Robbert <admin@bigrocketgames.com>
2017-07-23 16:56:16 -0700

    Corrected a typo in the validation for the message model.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,6 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
 
-  validates :content, :user_id, :chatRoom_id, presence: true
+  validates :content, :user_id, :chat_room_id, presence: true
   validates :content, length: { minimum: 1 }
 end
