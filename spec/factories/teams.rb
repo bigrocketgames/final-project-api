fdf773b344f584dc38bf55a2327d7430add0ab8b
Nathan Robbert <admin@bigrocketgames.com>
2017-07-22 20:14:09 -0700

    Added home team and away team child factories to team factory.

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -1,7 +1,19 @@
 FactoryGirl.define do
-  factory :team, aliases: [:home_team, :away_team] do
+  factory :team do
     name "Seattle"
     mascot "Seahawks"
     sport
+  end
+
+  factory :home_team, parent: :team do
+    name "Denver"
+    mascot "Broncos"
+    sport_id 1
+  end
+
+  factory :away_team, parent: :team do
+    name "Los Angeles"
+    mascot "Rams"
+    sport_id 1
   end
 end
