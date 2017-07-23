759d1392204391dcd010fa03590309a197784469
Nathan Robbert <admin@bigrocketgames.com>
2017-07-22 17:11:03 -0700

    Playing around with game factory.

--- spec/factories/games.rb
+++ spec/factories/games.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :game do
-    game_time "2017-07-22 15:34:55"
+    game_time "2017-07-22 19:00:00"
     home_team_id 1
-    away_team_id 1
+    away_team_id 2
     chat_room_id 1
   end
 end
