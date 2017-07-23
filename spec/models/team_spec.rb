2126ddc57fd1cf33acb4b6b0c5af7603fabec8cd
Nathan Robbert <admin@bigrocketgames.com>
2017-07-22 20:14:51 -0700

    Passing team spec for has many fans relationship.

--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -23,7 +23,13 @@ RSpec.describe Team, type: :model do
       expect(team.sport).to_not eq(nil)
     end
 
-    it 'has many fans through favorite teams'
+    it 'has many fans through favorite teams' do
+      team = create(:team)
+      team.fans.create(email: "test@test.test", password: "password", first_name: "Tom", last_name: "Tester")
+      team.fans.create(email: "test1@test.test", password: "password", first_name: "Tina", last_name: "Tester")
+
+      expect(team.fans.length).to eq(2)
+    end
   end
   
 end
