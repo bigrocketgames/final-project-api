4cc1c5aaef3a6a58cc89fb0abe9df0edadf3c9a5
Nathan Robbert <admin@bigrocketgames.com>
2017-07-22 17:22:32 -0700

    Updated belongs to relationship on message model from chatRoom to chat_room.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ApplicationRecord
   belongs_to :user
-  belongs_to :chatRoom
+  belongs_to :chat_room
 
   validates :content, :user_id, :chatRoom_id, presence: true
   validates :content, length: { minimum: 1 }
