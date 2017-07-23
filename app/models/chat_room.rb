2f680c780fef97411bb248d0cccf5af904885d23
Nathan Robbert <admin@bigrocketgames.com>
2017-07-22 20:10:19 -0700

    Updated chatroom model relationship to account for having many users through messages.

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -1,7 +1,7 @@
 class ChatRoom < ApplicationRecord
   belongs_to :game
   has_many :messages
-  has_many :users through: :messages
+  has_many :users, through: :messages
 
   validates :game_id, presence: true
 end
