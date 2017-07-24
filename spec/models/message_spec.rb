364683f71d9a49fbc312a9df3e9ec89f0abab45e
Nathan Robbert <admin@bigrocketgames.com>
2017-07-23 17:01:13 -0700

    Passing all validation and relationship specs for the Message model.

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,24 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
+
+  before(:each) do
+    @user = create(:user)
+    @sport = create(:sport)
+    @home_team = create(:home_team, sport_id: @sport.id)
+    @away_team = create(:away_team, sport_id: @sport.id)
+    @game = create(:game, home_team_id: @home_team.id, away_team_id: @away_team.id)
+    @chat_room = create(:chat_room, game_id: @game.id)
+  end
+
   describe 'validations' do
     it 'requires content, user_id, and chatRoom_id on creation' do
-      message = build(:message)
-      message1 = build(:message, user_id: nil, chatRoom_id: nil)
+      message = build(:message, user_id: @user.id, chat_room_id: @chat_room.id)
+      message1 = build(:message, user_id: nil, chat_room_id: nil)
 
       expect(message.valid?).to eq(true)
       expect(message1.valid?).to eq(false)
       expect(message1.errors.full_messages).to eq([
-        "User can't be blank",
-        "ChatRoom can't be blank"
+        "User must exist", 
+        "Chat room must exist", 
+        "User can't be blank", 
+        "Chat room can't be blank"
       ])
     end
 
     it 'requires the content to be at least 1 character in length' do
-      message = build(:message, content: nil)
+      message = build(:message, content: nil, user_id: @user.id, chat_room_id: @chat_room.id)
 
       expect(message.valid?).to eq(false)
       expect(message.errors.full_messages).to eq([
+        "Content can't be blank",
         "Content is too short (minimum is 1 character)"
       ])
     end
@@ -26,9 +39,17 @@ RSpec.describe Message, type: :model do
   end
 
   describe 'relationships' do
-    it 'belongs_to a user' 
+    it 'belongs_to a user' do
+      message = build(:message, content: nil, user_id: @user.id, chat_room_id: @chat_room.id)
 
-    it 'belongs_to a chatRoom'
-    
+      expect(message.user).to_not eq(nil)
+    end
+
+    it 'belongs_to a chatRoom' do
+      message = build(:message, content: nil, user_id: @user.id, chat_room_id: @chat_room.id)
+
+      expect(message.chat_room).to_not eq(nil)
+    end
+
   end
 end
