189df4e6ab3882a92c67e97c90970fd8f02a1ac7
Nathan Robbert <admin@bigrocketgames.com>
2017-07-26 12:47:25 -0700

    Passing validation tests for user_team model.

--- spec/models/user_team_spec.rb
+++ spec/models/user_team_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe UserTeam, type: :model do
+  describe 'validations' do
+    it 'requires a team_id and user_id' do
+      user_team = build(:user_team, user_id: nil, team_id: nil)
+
+      expect(user_team.valid?).to eq(false)
+      expect(user_team.errors.full_messages).to eq([
+        "User must exist", 
+        "Team must exist", 
+        "User can't be blank", 
+        "Team can't be blank"
+      ])
+    end
+  end
+
   describe 'relationships' do
     it 'belongs to a user' do
       user_team = create(:user_team)
