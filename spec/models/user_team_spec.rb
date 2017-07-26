c890b6f9f0bd5f29ea39034efdbe568c0bcbc550
Nathan Robbert <admin@bigrocketgames.com>
2017-07-26 10:02:02 -0700

    User_team model passing relationship tests for belonging to user and belonging to team.

--- spec/models/user_team_spec.rb
+++ spec/models/user_team_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UserTeam, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it 'belongs to a user' do
+      user_team = create(:user_team)
+
+      expect(user_team.user.id).to_not eq(nil)
+    end
+
+    it 'belongs to a team' do
+      user_team = create(:user_team)
+
+      expect(user_team.team.id).to_not eq(nil)
+    end
+  end
 end
