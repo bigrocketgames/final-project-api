dcefc36012cd4a3c1d7e4d4d71515b83cc18ba9e
Nathan Robbert <admin@bigrocketgames.com>
2017-07-26 17:07:10 -0700

    Updated team model to reflect association change of belonging to sub_sport from sport.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,13 +1,13 @@
 class Team < ApplicationRecord
-  belongs_to :sport
+  belongs_to :sub_sport
   has_many :user_teams
   has_many :fans, through: :user_teams, source: :user
 
-  validates :name, :mascot, :sport, presence: true
+  validates :name, :mascot, :sub_sport, presence: true
   validate :no_duplicate_team
 
   def no_duplicate_team
-    teamExists = Team.where('name = ? AND mascot = ? AND sport_id = ?', self.name, self.mascot, self.sport).count
+    teamExists = Team.where('name = ? AND mascot = ? AND sub_sport_id = ?', self.name, self.mascot, self.sub_sport).count
     if teamExists > 0
       errors.add(:name, "can't have a duplicate team")
     end
