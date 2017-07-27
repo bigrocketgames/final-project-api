baf82b637a5bc520a079d2893153fd3f77186ef0
Nathan Robbert <admin@bigrocketgames.com>
2017-07-26 17:46:55 -0700

    Changed has many to sub_sports instead of teams.

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -1,5 +1,5 @@
 class Sport < ApplicationRecord
-  has_many :teams, dependent: :destroy
+  has_many :sub_sports, dependent: :destroy
   
   validates :sport_name, presence: true, uniqueness: true
 end
