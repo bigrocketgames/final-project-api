a74fd3ec7d3e2f093b72a672e36ec18af06f6b4b
Nathan Robbert <admin@bigrocketgames.com>
2017-07-27 09:27:12 -0700

    Added name column to chat_rooms table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170726233337) do
+ActiveRecord::Schema.define(version: 20170727161951) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170726233337) do
     t.integer  "game_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "games", force: :cascade do |t|
