1e697fee99f050acf80daf234b601944ff2ff382
Nathan Robbert <admin@bigrocketgames.com>
2017-07-26 17:08:50 -0700

    Updated team factory to reflect new association with sub_sport from sport.

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -2,18 +2,18 @@ FactoryGirl.define do
   factory :team do
     name "Seattle"
     mascot "Seahawks"
-    sport
+    sub_sport
   end
 
   factory :home_team, parent: :team do
     name "Denver"
     mascot "Broncos"
-    sport_id 1
+    sub_sport_id 1
   end
 
   factory :away_team, parent: :team do
     name "Los Angeles"
     mascot "Rams"
-    sport_id 1
+    sub_sport_id 1
   end
 end
