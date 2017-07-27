0ad6b5db7f49b227426792316aec4074f8eb04aa
Nathan Robbert <admin@bigrocketgames.com>
2017-07-27 11:56:48 -0700

    Added private callback method to add the name of the chatroom after_create.

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -3,5 +3,15 @@ class ChatRoom < ApplicationRecord
   has_many :messages
   has_many :users, through: :messages
 
+  after_create :add_name
   validates :game_id, uniqueness: true, presence: true
+
+  private
+
+  def add_name
+    gameDay = self.game.game_time.strftime(" %A %m/%d/%Y")
+    roomName = self.game.away_team.name + " " + self.game.away_team.mascot + " @ " + self.game.home_team.name + " " + self.game.home_team.mascot + gameDay
+
+    self.update(name: roomName)
+  end
 end
