3ca448642428a244eb110d9d3e3dfb7add0ca907
Nathan Robbert <admin@bigrocketgames.com>
2017-08-17 19:10:16 -0700

    Still playing with chat_room name function.

--- app/models/chat_room.rb
+++ app/models/chat_room.rb
@@ -10,7 +10,11 @@ class ChatRoom < ApplicationRecord
 
   def add_name
     gameDay = self.game.game_time.strftime(" %A %m/%d/%Y")
-    roomName = self.game.away_team.name + " " + self.game.away_team.mascot + " @ " + self.game.home_team.name + " " + self.game.home_team.mascot + gameDay
+    # roomName = self.game.away_team.name + " " + self.game.away_team.mascot + " @ " + self.game.home_team.name + " " + self.game.home_team.mascot + gameDay
+
+    # roomName = self.game.away_team.fullName + " @ " + self.game.home_team.fullName + gameDay
+
+    binding.pry
 
     self.update(name: roomName)
   end
