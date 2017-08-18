4db806730fd86aa7403c109f1f3b79fc62c61443
Nathan Robbert <admin@bigrocketgames.com>
2017-08-17 20:06:48 -0700

    Added home_location to team factories.

--- spec/factories/teams.rb
+++ spec/factories/teams.rb
@@ -2,18 +2,21 @@ FactoryGirl.define do
   factory :team do
     name "Seattle"
     mascot "Seahawks"
+    home_location "Seattle, WA"
     sub_sport
   end
 
   factory :home_team, parent: :team do
     name "Denver"
     mascot "Broncos"
+    home_location "Denver, CO"
     sub_sport
   end
 
   factory :away_team, parent: :team do
     name "Los Angeles"
     mascot "Rams"
+    home_location "Los Angeles, CA"
     sub_sport
   end
 end
