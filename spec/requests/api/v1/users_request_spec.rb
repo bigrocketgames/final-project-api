fa263f500f9b3a03e82871e474458c0e7f398971
Nathan Robbert <admin@bigrocketgames.com>
2017-08-25 20:01:25 -0700

    Began writing request tests for users_controller.

--- spec/requests/api/v1/users_request_spec.rb
+++ spec/requests/api/v1/users_request_spec.rb
@@ -5,7 +5,26 @@ RSpec.describe "Api::V1::Users", Type: :request do
   describe "POST /users" do
 
     describe "on success" do
-      it "creates a user from the params" 
+      it "creates a user from the params" do
+        params = {
+          user: {
+            email: "test@test.com",
+            password: "password"
+          }
+        }
+
+        post "/api/v1/users",
+          params: params.to_json,
+          headers: { 'Content-Type': 'application/json' }
+
+        body = JSON.parse(response.body)
+        
+        expect(response.status).to eq(200)
+        expect(body['user']['id']).not_to eq(nil)
+        expect(body['user']['email']).to eq("test@test.com")
+        expect(body['user']['password_digest']).to eq(nil)
+        expect(body['token']).not_to eq(nil)
+      end
       
       it "returns the new user and a JWT token" 
     end
