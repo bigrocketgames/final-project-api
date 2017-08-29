01be06d5c5123240e61f4254f5351f085d3136ed
Nathan Robbert <admin@bigrocketgames.com>
2017-08-28 20:56:58 -0700

    Passing tests for post api/v1/refresh

--- spec/requests/api/v1/auth_request_spec.rb
+++ spec/requests/api/v1/auth_request_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Auth", Type: :request do
 
-  describe "POST /auth" do
+  before(:each) do
+    @user = User.create(
+      email: "test@test.com",
+      password: "password"
+    )
+  end
 
-    before(:each) do
-      User.create(
-        email: "test@test.com",
-        password: "password"
-      )
-    end
+  describe "POST /auth" do
 
     describe "on success" do
       
-      it "returns the existing user and a JWT token" do
+      it "returns the existing user (from params) and a JWT token" do
         params = {
           user: {
             email: "test@test.com",
@@ -79,6 +79,51 @@ RSpec.describe "Api::V1::Auth", Type: :request do
 
     end
     
+  end
+
+  describe "POST /auth/refresh" do
+
+    describe "on success" do
+      
+      it "returns the existing user (from the headers JWT) and a JWT token" do
+        token = Auth.create_token(@user.id)
+        
+        post "/api/v1/auth/refresh",
+          headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer: #{token}"
+        }
+
+        body = JSON.parse(response.body)
+        
+        expect(response.status).to eq(200)
+        expect(body['user']['id']).not_to eq(nil)
+        expect(body['user']['email']).to eq("test@test.com")
+        expect(body['user']['password_digest']).to eq(nil)
+        expect(body['token']).not_to eq(nil)
+      end
+    end
+
+    describe "on error" do
+      
+      it "unable to find user with email" do
+        token = 'abc.123.desf.sdf'
+        
+        post "/api/v1/auth/refresh",
+          headers: { 
+            'Content-Type': 'application/json',
+            'Authorization': "Bearer: #{token}"
+        }
+
+        body = JSON.parse(response.body)
+
+        expect(response.status).to eq(403)
+        expect(body["errors"]).to eq(
+          [{"message"=>"Token is invalid!"}]
+        )
+      end
+    end
+
   end
 
 end
