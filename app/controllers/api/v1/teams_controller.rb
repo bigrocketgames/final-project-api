c7a5bc3f01c3777192d07edf01b511f707a25f13
Nathan Robbert <admin@bigrocketgames.com>
2017-09-01 18:58:20 -0700

    Creating team controller.

---
+++ app/controllers/api/v1/teams_controller.rb
@@ -0,0 +1,33 @@
+class Api::V1::TeamsController < ApplicationController
+
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      render json: @team
+    else
+      render json: {
+        errors: @team.errors
+      }, status: 400
+    end
+  end
+
+  def update
+    @team = Team.find(params[:id])
+
+    if @team.update(team_params)
+      render json: @team
+    else
+      render json: {
+        errors: @team.errors
+      }, status: 400
+    end
+  end
+
+  private
+    
+  def team_params
+    params.require(:team).permit(:name, :mascot, :sub_sport_id, :home_location, :fullName)
+  end
+
+end
