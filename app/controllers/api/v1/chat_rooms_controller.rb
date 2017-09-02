c9f2cceff0907dddd7a68ccda5c6cd7a2bc1e709
Nathan Robbert <admin@bigrocketgames.com>
2017-09-01 20:02:03 -0700

    Updated chat rooms controller for create and update.

--- app/controllers/api/v1/chat_rooms_controller.rb
+++ app/controllers/api/v1/chat_rooms_controller.rb
@@ -1,5 +1,31 @@
 class Api::V1::ChatRoomsController < ApplicationController
 
-  
+  def create
+    chatRoom = ChatRoom.new(chat_room_params)
 
+    if chatRoom.save
+      render json: chatRoom
+    else
+      render json: {
+        errors: chatRoom.errors
+      }, status: 400
+    end
+  end
+
+  def update
+    chatRoom = ChatRoom.find(params[:id])
+
+    if chatRoom.update(chat_room_params)
+      render json: chatRoom
+    else
+      render json: {
+        errors: chatRoom.errors
+      }, status: 400
+    end
+
+  private
+
+  def chat_room_params
+    params.require(:chat_room).permit(:game_id)
+  end
 end
